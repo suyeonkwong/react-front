fdcdcfccb58716cc2255de181caa644d58c5e27d
LAPTOP-BP7K41TK\82103 <rnjstndus49@gmail.com>
2023-02-12 01:41:53 +0900

    router

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
 import logo from './logo.svg';
 import './App.css';
+import {Container, Nav, Navbar} from "react-bootstrap";
+import {useState} from "react";
+import data from './data';
+import { Routes, Route, Link} from "react-router-dom";
 
 function App() {
+
+    let [fruit] = useState(data)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+        <Navbar bg="dark" variant="dark">
+            <Container>
+                <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+                <Nav className="me-auto">
+                    <Nav.Link href="#home">Home</Nav.Link>
+                    <Nav.Link href="#features">Features</Nav.Link>
+                    <Nav.Link href="#pricing">Pricing</Nav.Link>
+                </Nav>
+            </Container>
+        </Navbar>
+
+        <Link to="/">홈</Link>
+        <Link to="detail">상세</Link>
+
+        <Routes>
+            <Route path="/detail" element={<div>상세페이지</div>} />
+            <Route path="/" element={<div>
+
+                <div className = "main-bg"></div>
+
+                <div className="container">
+                    <div className="row">
+                        {
+                            fruit.map(function(a, i) {
+                                return (
+                                    <Fruit fruit={fruit[i]}></Fruit>
+                                )
+                            })
+                        }
+                    </div>
+                </div>
+
+            </div>}/>
+        </Routes>
+
     </div>
   );
+}
+
+function Fruit(props) {
+    return (
+        <div className="col-md-4">
+            <img src={process.env.PUBLIC_URL + props.fruit.src} width="80%"/>
+            <h4>{props.fruit.title}</h4>
+            <h4>{props.fruit.price}</h4>
+        </div>
+    )
 }
 
 export default App;
---
+++ src/data.js
@@ -0,0 +1,27 @@
+let data = [
+    {
+        id : 0,
+        title : "White and Black",
+        content : "Born in France",
+        price : 120000,
+        src : '/img/strawberry.jpg'
+    },
+
+    {
+        id : 1,
+        title : "Red Knit",
+        content : "Born in Seoul",
+        price : 110000,
+        src : '/img/orange.jpg'
+    },
+
+    {
+        id : 2,
+        title : "Grey Yordan",
+        content : "Born in the States",
+        price : 130000,
+        src : '/img/cherry.jpeg'
+    }
+]
+
+export default data;
